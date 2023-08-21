1d0a7183c8468b18e060d61fd625dbf15066aa76
VyVy99 <dinhhavy2406@gmail.com>
2023-08-21 18:44:52 +0700

    finish react 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Banner from "./BaiTapLayoutComponent/Banner";
+import Body from "./BaiTapLayoutComponent/Body";
+
+import Header from "./BaiTapLayoutComponent/Header";
+import Item from "./BaiTapLayoutComponent/Item";
+import Footer from "./BaiTapLayoutComponent/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<Header />
+<div>
+
+<Banner />
+
+< Item />
+< Footer />
+
+
+
+<Body />
+
+
+
+</div>
+
+
     </div>
   );
 }
---
+++ src/BaiTapLayoutComponent/Item.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react'
+
+export default class Item extends Component {
+    render() {
+        return (
+            <div>
+              <div className="container">
+  <div className="row ">
+    <div className="col-3">
+      <div className="card h-100">
+        <div className="p-5 text-dark text-center bg-secondary  fs-4 w-100" style={{padding: 100}}>550X325</div>
+        <div className="card-body" style={{height: 300}}>
+          <h5 className=" fs-3 text-center card-title">Card title</h5>
+          <p className="card-text text-center">This is a wider card with supporting text below as a
+            natural lead-in to
+            additional
+            content. This content is a little bit longer.</p>
+        </div>
+        <div className="card-footer  bg-light rounded-3 text-center">
+          <a className="btn btn-primary btn-lg" href="#!">Call to action</a>
+        </div>
+      </div>
+    </div>
+    <div className="col-3">
+      <div className="card h-100">
+        <div className="p-5 text-dark text-center bg-secondary  fs-4 w-100" style={{padding: 100}}>550X325</div>
+        <div className="card-body" style={{height: 300}}>
+          <h5 className=" fs-3 text-center card-title">Card title</h5>
+          <p className="card-text text-center">This is a wider card with supporting text below as a
+            natural lead-in to
+            additional
+            content. This content is a little bit longer.</p>
+        </div>
+        <div className="card-footer  bg-light rounded-3 text-center">
+          <a className="btn btn-primary btn-lg" href="#!">Call to action</a>
+        </div>
+      </div>
+    </div>
+    <div className="col-3">
+      <div className="card h-100">
+        <div className="p-5 text-dark text-center bg-secondary  fs-4 w-100" style={{padding: 100}}>550X325</div>
+        <div className="card-body" style={{height: 300}}>
+          <h5 className=" fs-3 text-center card-title">Card title</h5>
+          <p className="card-text text-center">This is a wider card with supporting text below as a
+            natural lead-in to
+            additional
+            content. This content is a little bit longer.</p>
+        </div>
+        <div className="card-footer  bg-light rounded-3 text-center">
+          <a className="btn btn-primary btn-lg" href="#!">Call to action</a>
+        </div>
+      </div>
+    </div>
+    <div className="col-3">
+      <div className="card h-100">
+        <div className="p-5 text-dark text-center bg-secondary  fs-4 w-100" style={{padding: 100}}>550X325</div>
+        <div className="card-body" style={{height: 300}}>
+          <h5 className=" fs-3 text-center card-title">Card title</h5>
+          <p className="card-text text-center">This is a wider card with supporting text below as a
+            natural lead-in to
+            additional
+            content. This content is a little bit longer.</p>
+        </div>
+        <div className="card-footer  bg-light rounded-3 text-center">
+          <a className="btn btn-primary btn-lg" href="#!">Call to action</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+            </div>
+        )
+    }
+}
